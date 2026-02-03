ddd734c4fd231e0169a5e2eb62ddfd20f52ea275
sunnuay <87159155+sunnuay@users.noreply.github.com>
2026-02-03 11:26:30 +0800

    Modify register function to only return failed status

--- app/(auth)/actions.ts
+++ app/(auth)/actions.ts
@@ -55,30 +55,5 @@ export const register = async (
   _: RegisterActionState,
   formData: FormData
 ): Promise<RegisterActionState> => {
-  try {
-    const validatedData = authFormSchema.parse({
-      email: formData.get("email"),
-      password: formData.get("password"),
-    });
-
-    const [user] = await getUser(validatedData.email);
-
-    if (user) {
-      return { status: "user_exists" } as RegisterActionState;
-    }
-    await createUser(validatedData.email, validatedData.password);
-    await signIn("credentials", {
-      email: validatedData.email,
-      password: validatedData.password,
-      redirect: false,
-    });
-
-    return { status: "success" };
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      return { status: "invalid_data" };
-    }
-
-    return { status: "failed" };
-  }
+  return { status: "failed" };
 };
